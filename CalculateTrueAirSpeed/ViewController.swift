afa5c9a5fa4e96dd93b38cf06cb8d6136e49fd57
nazmiyavuz <nazmi_yavuz@hotmail.com>
2021-06-23 17:48:34 +0300

    Start creating calculation

--- CalculateTrueAirSpeed/ViewController.swift
+++ CalculateTrueAirSpeed/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - UIView
+    
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - LifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    // MARK: - Button Actions
 
 }
 
